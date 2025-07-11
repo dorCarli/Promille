bcd3f9d8d0578799335650471e7ad1e08e86073f
dorCarli <hannes0103.ca2006@gmail.com>
2025-07-11 11:01:55 +0200

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD43TYRuIZxI1pS_noOzlKCIEzUm8Q7FiQ",
-  authDomain: "promille-b4bd3.firebaseapp.com",
-  projectId: "promille-b4bd3",
-  messagingSenderId: "627353030877",
-  appId: "1:627353030877:web:218cd951414fe996bbcb34"
+  apiKey: "DEIN_API_KEY",
+  authDomain: "DEINE_DOMAIN.firebaseapp.com",
+  projectId: "DEIN_PROJECT_ID",
+  messagingSenderId: "DEINE_SENDER_ID",
+  appId: "DEINE_APP_ID",
 });
 
 const messaging = firebase.messaging();
+
+// Push-Nachrichten-Empfang im Hintergrund:
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Empfangene Nachricht im Hintergrund:', payload);
+  // Hier Notification anzeigen, z.B.:
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png',
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
