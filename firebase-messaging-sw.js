2890b8718ef27b03b4b677ad1fbb5786c94c408a
dorCarli <hannes0103.ca2006@gmail.com>
2025-07-11 13:02:13 +0200

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,13 @@
-self.addEventListener('push', function(event) {
-  const payload = event.data?.json() || {};
-  const title = payload.notification?.title || 'Neue Nachricht';
-  const options = {
-    body: payload.notification?.body || '',
-    icon: '/Promille/icon-192.png', // oder dein eigenes Icon
-  };
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
 
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyD43TYRuIZxI1pS_noOzlKCIEzUm8Q7FiQ",
+  authDomain: "promille-b4bd3.firebaseapp.com",
+  projectId: "promille-b4bd3",
+  storageBucket: "promille-b4bd3.firebasestorage.app",
+  messagingSenderId: "627353030877",
+  appId: "1:627353030877:web:18285915baa3744ebbcb34",
 });
+
+const messaging = firebase.messaging();
